c2ede354d41284507cdea5ba8d689ab05813cf3d
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-04 12:20:05 -0500

    feat: add airplanes assets

---
+++ README.md
@@ -0,0 +1,6 @@
+## airplanes
+
+A simple airplanes based game written in Go to learn about EbitEngine
+and game developement.
+
+
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/hajimehoshi/ebiten"
+
+type Game struct {
+}
+
+// game events
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	g := &Game{}
+
+	err := ebiten.RunGame(g)
+	if err != nil {
+		panic(err)
+	}
+}
